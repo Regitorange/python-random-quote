ae24380dc88542d5f435b3c8cab6540b716eadde
Regit <regitorange@live.com>
2021-12-23 16:42:47 +0800

    close

--- get-quote.py
+++ get-quote.py
@@ -6,6 +6,10 @@ def primary():
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
+
+  last = len(quotes) - 1
+  rnd = random.randint(0, last)
+  print(quotes[rnd])
   
   last = len(quotes) - 1
   rnd = random.randint(0, last)
